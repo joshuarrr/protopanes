d5249f3fb9d7e41cce879bd1bc74f2e3d909fcc0
Joshua Richey <joshua.richey@aclimalabs.com>
2022-11-06 16:11:52 -0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import "the-new-css-reset/css/reset.css";
-import './index.css';
+import React from "react";
+import Panes from './components/Panes'
+import './App.css'
 
 function App() {
   return (
-    <div className="App dot-grid">
-      <header className="protopanes-header">protopanes</header>
-            
+    <div className="App dotgrid">
+      <header className="protopanes-header">
+        <h1 className="logotype text">protopanes</h1>
+      </header>
+      <main className="main">
+        <Panes />
+      </main>    
     </div>
   );
 }
 
 export default App;
+
+

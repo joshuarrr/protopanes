0bab39b81c22c20ecfc0718b64898843cab9d9eb
Joshua Richey <joshua.richey@aclimalabs.com>
2023-02-13 17:06:35 -0800

    uh huh..

--- src/components/Panes.js
+++ src/components/Panes.js
@@ -1,28 +1,137 @@
+import ReactDOM from 'react-dom'
+import React, { Component } from "react"
+import { ReflexContainer, ReflexSplitter, ReflexElement } from "react-reflex"
+import "react-reflex/styles.css"
 
-import React from 'react'
+class CollapsibleElementCls extends React.Component {
+  constructor(props) {
+    super(props)
+    this.wrapper = React.createRef()
+    this.state = {
+      collapseRight: false,
+      collapseLeft: false,
+    }
+  }
 
-import {
-  ReflexContainer,
-  ReflexSplitter,
-  ReflexElement
-} from 'react-reflex'
+  componentWillReceiveProps(nextProps) {
+    if (this.props.onCollapse && this.getSize() < this.props.threshold) {
+      this.props.onCollapse()
+    }
+  }
 
-import 'react-reflex/styles.css'
+  getSize() {
+    const container = ReactDOM.findDOMNode(this)
 
+    switch (this.props.orientation) {
+      case "horizontal":
+        return container.offsetHeight
+      case "vertical":
+        return container.offsetWidth
+      default:
+        return 0
+    }
+  }
 
-const Panes = () => {
-  return (
-      <ReflexContainer orientation="vertical">
-        <ReflexElement className="left-pane">
-          <div className="pane-content">Left Pane</div>
-        </ReflexElement>
-        <ReflexSplitter />
-        <ReflexElement className="right-pane">
-          <div className="pane-content">Right Pane</div>
+  render() {
+    return (
+      <ReflexElement {...this.props} ref={this.wrapper}>
+        <div className="pane-content">
+          <label>
+            I will collapse when I get smaller than &nbsp;{this.props.threshold}
+            px
+          </label>
+        </div>
+      </ReflexElement>
+    )
+  }
+}
+
+const CollapsibleElement = React.forwardRef((props, ref) => {
+  return <CollapsibleElementCls innerRef={ref} {...props} />
+})
+
+class Panes extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      collapseRight: false,
+      collapseLeft: false,
+      header: false,
+    }
+  }
+
+  collapseLeft(collapseLeft) {
+    this.setState({
+      ...this.state,
+      collapseLeft,
+      header: true,
+    })
+  }
+
+  collapseRight(collapseRight) {
+    this.setState({
+      ...this.state,
+      collapseRight,
+      header: true,
+    })
+  }
+
+  render() {
+
+  const collapsibleHeader = this.state.header &&
+          <ReflexElement className="header" minSize={30} maxSize={30}>
+            <div style={{ margin: "6px" }}>
+              {this.state.collapseLeft && (
+                <button onClick={() => this.collapseLeft(false)}>
+                  <label> Show Left Pane </label>
+                </button>
+              )}
+              {this.state.collapseRight && (
+                <button onClick={() => this.collapseRight(false)}>
+                  <label> Show Right Pane </label>
+                </button>
+              )}
+            </div>
+          </ReflexElement>
+
+    return (
+      <ReflexContainer orientation="horizontal">
+        {collapsibleHeader}
+       <ReflexElement>
+          <ReflexContainer orientation="vertical">
+            {!this.state.collapseLeft && (
+              <CollapsibleElement
+                className="left-pane"
+                onCollapse={() => this.collapseLeft(true)}
+                threshold={100}
+              />
+            )}
+
+            {!this.state.collapseLeft && <ReflexSplitter propagate={true} />}
+
+            <ReflexElement minSize={100} className="middle-pane">
+              <div className="pane-content">
+                <label>
+                  Minimum size: <br /> 100 px
+                </label>
+              </div>
+            </ReflexElement>
+
+            {!this.state.collapseRight && <ReflexSplitter propagate={true} />}
+
+            {!this.state.collapseRight && (
+              <CollapsibleElement
+                className="right-pane"
+                onCollapse={() => this.collapseRight(true)}
+                threshold={100}
+              />
+            )}
+          </ReflexContainer>
         </ReflexElement>
       </ReflexContainer>
-  )
+    )
+  }
 }
 
 export default Panes
-
